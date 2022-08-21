527644ef2cd3e85633049d4af583ea5a88cc3f03
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-21 09:19:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ TOKEN = os.getenv('5478645762:AAH4pRIpRCA6ky2ZoF-VeFO5cV6k28CAcj8')
 deadline = datetime(datetime.today().year, datetime.today().month, datetime.today().day, hour=14)
 VICTORY = 30
 victory_text = ''
-redis_server = redis.from_url(os.getenv('redis://redis-17314.c270.us-east-1-3.ec2.cloud.redislabs.com:17314'))
-PORT = int(os.environ.get('PORT', 5000))
+redis_server = redis.from_url(os.getenv('redis-17314.c270.us-east-1-3.ec2.cloud.redislabs.com:17314, redis://localhost:17314'))
+PORT = int(os.environ.get('PORT', 17314))
 
 
 def setup_shippering_db(update: Update, context: CallbackContext):
