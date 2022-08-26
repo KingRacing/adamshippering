0b00d6f92ba06718ae423e142a9b24c922bc9c53
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-26 07:13:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ VICTORY = 30
 victory_text = ''
 
 listen = ['default']
-redis_url = os.strip('redis-17314.c270.us-east-1-3.ec2.cloud.redislabs.com:17314', 'redis://localhost:17314')
+redis_url = os.getenv('redis-17314.c270.us-east-1-3.ec2.cloud.redislabs.com:17314', 'redis://localhost:17314')
 if __name__ == '__main__':
     print(f"redis url: {redis_url}")
     with Connection(redis.from_url(redis_url)):
